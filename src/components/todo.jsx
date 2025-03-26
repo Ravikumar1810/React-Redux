de27d427a90c01a9d2b21928dbf22c3cb2f3fb11
Ravikumar1810 <nkravikumar18@gmail.com>
2025-03-27 00:30:05 +0530

    Todo list updated

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,11 +1,31 @@
 import { useSelector } from "react-redux";
+import AddForm from "./addForm";
+import { useDispatch } from "react-redux";
+import { deleteTodo } from "../features/todo/todoSlice";
 
  const Todo = () =>  {
+
+
      const todos = useSelector((state) => state.todos);
+     const dispatch = useDispatch();
      console.log(todos);
+     
+
+
+     const clickHandler = (id) => {
+        console.log("Deleted", id);
+        dispatch(deleteTodo(id));
+     }
     return(
         <>
-        <h2>Todos</h2>
+        <AddForm />
+        <h2>Todo List App</h2>
+        <ul>
+            {todos.map((todo) => (
+                <li key={todo.id}>{todo.task}
+                <button onClick={() => clickHandler(todo.id)}>delete</button>
+                </li>))}
+        </ul>
         </>
     );
 }
